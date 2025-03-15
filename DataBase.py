2ba47175796eb9093cd784f0464224a5f513de12
demetrio <lisovd2006@yandex.ru>
2025-03-15 20:38:42 +0300

    Added database

---
+++ DataBase.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+con = sqlite3.connect("DataBase.db")
+cur = con.cursor()
+
+
+def create_db():
+    cur.execute("""CREATE TABLE "person" (
+                    "id"	INTEGER NOT NULL UNIQUE,
+                    "name"	,
+                    "passhash"	,
+                    PRIMARY KEY("id" AUTOINCREMENT)
+                    );""")
+
+
+def get_hash(s):
+    return "a"
+
+
+def create_person(name, password):
+    cur.execute("""
+                INSERT INTO person VALUES
+                (?, ?, ?)
+                """, (1, name, get_hash(password)))
+    con.commit()
+
+
+# create_db()
+create_person(name="Test", password="TestPass")
