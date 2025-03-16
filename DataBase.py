8c8e618b02d3c83c85da04da3113f49d12e716a2
demetrio <lisovd2006@yandex.ru>
2025-03-16 14:41:03 +0300

    Added base functional

--- DataBase.py
+++ DataBase.py
@@ -1,34 +1,63 @@
 import sqlite3
-
-con = sqlite3.connect("DataBase.db")
-cur = con.cursor()
+import datetime
 
 
-def create_db():
-    cur.execute("""CREATE TABLE "person" (
-                    "id"	INTEGER NOT NULL UNIQUE,
-                    "name"	,
-                    "passhash"	,
-                    PRIMARY KEY("id" AUTOINCREMENT)
-                    );""")
-    cur.execute("""CREATE TABLE "info" (
-                "id",
-                "reg_time",
-                "last_auth"
-                );""")
+class Database:
+    def __init__(self):
+        self.con = sqlite3.connect("DataBase.db")
+        self.cur = self.con.cursor()
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS "person" (
+                        "id"	INTEGER NOT NULL UNIQUE,
+                        "name"	TEXT NOT NULL,
+                        "passhash"	TEXT NOT NULL,
+                        PRIMARY KEY("id" AUTOINCREMENT)
+                        );""")
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS "info" (
+                        "id" INTEGER NOT NULL UNIQUE,
+                        "reg_time" DATETIME DEFAULT CURRENT_TIMESTAMP,
+                        "last_auth" DATETIME,
+                        PRIMARY KEY ("id" AUTOINCREMENT)
+                        );""")
+        self.con.commit()
 
+    def create_person(self, name, password):
+        self.cur.execute("""INSERT INTO person (name, passhash) VALUES
+                    (?, ?)
+                    """, (name, password))
+        self.cur.execute("""INSERT INTO info (reg_time, last_auth) VALUES
+                    (?, ?)""", (str(datetime.datetime.now()), str(datetime.datetime.now())))
+        self.con.commit()
 
-def get_hash(s):
-    return "a"
+    def __get_by_id(self, id):
+        get_response = self.cur.execute("""
+                                    SELECT id, name FROM person WHERE id = ?""", str(id))
+        return get_response.fetchall()
 
+    def find_by_name(self, name: str) -> list:
+        get_response = self.cur.execute("""
+            SELECT * FROM person WHERE name = ?
+        """, (name,))
+        return get_response.fetchall()
 
-def create_person(name, password):
-    cur.execute("""
-                INSERT INTO person VALUES
-                (?, ?, ?)
-                """, (1, name, get_hash(password)))
-    con.commit()
+    def delete_by_name(self, name: str):
+        try:
+            id = self.cur.execute("""
+                SELECT * FROM person WHERE name = ?
+            """, (name,)).fetchone()[0]
+            self.__delete_person(id)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
+    def __delete_person(self, id: int):
+        self.cur.execute("""
+            DELETE FROM info WHERE id = ?
+        """, (str(id),))
+        self.cur.execute("""
+            DELETE FROM person WHERE id = ?
+        """, (str(id),))
+        self.con.commit()
 
-# create_db()
-create_person(name="Test", password="TestPass")
+    def close(self):
+        self.con.close()
---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, JSONResponse
+from DataBase import Database
+from hashlib import sha256
+
+templates = Jinja2Templates(directory="templates")
+
+app = FastAPI()
+
+tasks = []
+
+
+db = Database()
+
+
+def password_hash(s: str) -> str:
+    return sha256(s.encode('utf-8')).hexdigest()
+
+
+@app.get("/login", response_class=HTMLResponse)
+async def get_login_form(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/login", response_class=HTMLResponse)
+async def post_root(login: str = Form(None), password: str = Form(None)):
+    try:
+        db.create_person(name=login, password=password_hash(password))
+        return f"Success"
+    except Exception as e:
+        return f"{str(e)}"
+
+# db.close()
