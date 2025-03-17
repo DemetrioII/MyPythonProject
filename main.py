a0520786d216d33b9736019ed4d2841bd9170c68
demetrio <lisovd2006@yandex.ru>
2025-03-17 14:21:13 +0300

    Added registration

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 tasks = []
 
-
 db = Database()
 
 
@@ -26,12 +25,27 @@ async def get_login_form(request: Request):
 
 
 @app.post("/login", response_class=HTMLResponse)
-async def post_root(login: str = Form(None), password: str = Form(None)):
+async def post_login_form(login: str = Form(None), password: str = Form(None)):
     try:
         hash_from_db = db.get_password_by_name(name=login)
         if hash_from_db == password_hash(password):
             return f"Success"
         return f"Wrong Password"
+    except Exception as e:
+        return f"{str(e)}"
+
+
+@app.get("/register", response_class=HTMLResponse)
+async def get_register_form(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+
+@app.post("/register", response_class=HTMLResponse)
+async def post_register_form(login: str = Form(None), password: str = Form(None), password_repeat: str = Form(None)):
+    try:
+        if password != password_repeat:
+            return f"Passwords are different"
+        db.create_person(login, password_hash(password))
     except Exception as e:
         return f"{str(e)}"
 
